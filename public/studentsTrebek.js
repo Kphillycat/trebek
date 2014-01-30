da6bd43e250eca493b1797f64233606521face3b
Kphillycat <keithwilliamsjr@gmail.com>
2014-01-30 18:59:39 -0500

    Add event handler to highlight playable non-category card when clicked on and disable all the other cards

---
+++ public/studentsTrebek.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	var $card = $(".card").not(".cat-card");
+	
+	$card.on("click", function(){	
+		$card.not(this).addClass("disabled");
+		$(this).not(".disabled").addClass("chosen");
+	});
+
+});
