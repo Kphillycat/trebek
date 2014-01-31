98fbae9cdf9eedc892900d4de3d8b8b5ee8a6072
Kphillycat <keithwilliamsjr@gmail.com>
2014-01-30 19:54:37 -0500

    Show question text when card is clicked on after being chosen

--- public/studentsTrebek.js
+++ public/studentsTrebek.js
@@ -1,10 +1,22 @@
 $(document).ready(function(){
 	var $card = $(".card").not(".cat-card");
+	var numClicksOnCard = 0;
 	
 	$card.on("click", function(){	
 		$card.not(this).addClass("disabled");
 		$(this).not(".disabled").addClass("chosen");
 		$(".up, .down").removeClass("disabled");
+
+		var $cardQuestion = $(".chosen"); //This is weird but it doesn't evaluate outside the function for the initial card click
+		$cardQuestion.on("click", function(){	
+			var $question = $(this).first().children().filter(".question");
+			var $value = $(this).first().children().filter(".value");
+			var $answer = $(this).first().children().filter(".value");
+			$value.text($question.text()); //set the value of value to the text in the question
+			numClicksOnCard = 1; //I don't know how else to know when the card is activated with the question
+		});
 	});
+	
+
 
 });
